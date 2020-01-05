b5eec9e6c584d66e47c858165438c222c3de706f
Liberbell <liberbell@gmail.com>
2020-01-06 07:52:52 +0900

    digital_makeup2

--- Ch07/digital_makeup.py
+++ Ch07/digital_makeup.py
@@ -18,12 +18,13 @@ for face_landmarks in face_landmarks_list:
     #  - chin, left_eyebrow, right_eyebrow, nose_bridge, nose_tip, left_eye, right_eye, top_lip, bottom_lip
 
     # Draw a line over the eyebrows
-
+    d.line(face_landmarks["left_eyebrow"], fill=(128, 0, 128, 100), width=3)
+    d.line(face_landmarks["right_eyebrow"], fill=(128, 0, 128, 100), width=3)
 
 
     # Draw over the lips
-
-
+    d.polygon(face_landmarks["top_lip"], fill(128, 0, 128, 100))
+    d.polygon(face_landmarks["bottom_lip"], fill(128, 0, 128, 100))
 
 
 # Show the final image
