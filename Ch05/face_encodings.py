af48b8c51d233d443f854425702305af77933c7b
Liberbell <liberbell@gmail.com>
2020-01-05 10:54:28 +0900

    face_encodings2

--- Ch05/face_encodings.py
+++ Ch05/face_encodings.py
@@ -1,10 +1,10 @@
-
+import face_recognition
 
 # Load the jpg files into numpy arrays
-
+image = face_recognition.load_image_file("person.jpg")
 
 # Generate the face encodings
-
+face_recognition.face_encodings(image)
 
 if len(face_encodings) == 0:
     # No faces found in the image.
