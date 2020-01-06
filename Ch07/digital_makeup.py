92ccc9bb51994bd4a1caa9a49bbf66a8a54c74c3
Liberbell <liberbell@gmail.com>
2020-01-07 07:37:02 +0900

    digital_makeup1

--- Ch07/digital_makeup.py
+++ Ch07/digital_makeup.py
@@ -14,18 +14,10 @@ pil_image = Image.fromarray(image)
 d = ImageDraw.Draw(pil_image, 'RGBA')
 
 for face_landmarks in face_landmarks_list:
-    # The face landmark detection model returns these features:
-    #  - chin, left_eyebrow, right_eyebrow, nose_bridge, nose_tip, left_eye, right_eye, top_lip, bottom_lip
-
-    # Draw a line over the eyebrows
     d.line(face_landmarks["left_eyebrow"], fill=(128, 0, 128, 100), width=3)
     d.line(face_landmarks["right_eyebrow"], fill=(128, 0, 128, 100), width=3)
-
-
-    # Draw over the lips
     d.polygon(face_landmarks["top_lip"], fill=(128, 0, 128, 100))
     d.polygon(face_landmarks["bottom_lip"], fill=(128, 0, 128, 100))
-
 
 # Show the final image
 pil_image.show()
